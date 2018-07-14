c16a0a3d3ad6f358fdd7046b7182500ff977faf2
Ali Abdukarim <aligorithm@icloud.com>
2018-07-13 18:01:20 -0700

    Added TestsVeiwCOntroller Did the half UI

---
+++ certprep/TestsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TestsViewController.swift
+//  certprep
+//
+//  Created by Alisher Abdukarimov on 7/13/18.
+//  Copyright © 2018 Alisher Abdukarimov. All rights reserved.
+//
+
+import UIKit
+
+class TestsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dissmissView(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
